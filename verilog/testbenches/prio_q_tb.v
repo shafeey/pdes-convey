a8a14b88064bb7aa7ce881906cf748646e5ea574
Shafiur Rahman <shafeeyrahman@gmail.com>
2016-07-23 11:41:55 +0600

    Rewrite to get test patterns from files automally

--- verilog/testbenches/prio_q_tb.v
+++ verilog/testbenches/prio_q_tb.v
@@ -1,552 +1,85 @@
 `timescale 1ns / 1ps
 
-`define DWIDTH 16 // MSB of DATA BUS
-`define HDEPTH 5	// Depth of heap
+`define DWIDTH 16 // Width of DATA BUS
+`define HDEPTH 5  // Depth of heap
 
 module prio_q_tb( );
 
-	reg clk, rst, enq, deq;
-    reg [`DWIDTH-1:0] inp_data;
-    wire [`DWIDTH-1:0] out_data;
-    wire [`HDEPTH-1:0] count;
-	
-	reg done;
-	
-	reg [`DWIDTH-1:0] min;
-	
-	always @ (posedge clk) begin
-		// if (deq) min <= out_data;
-		// else min <= 'x;
-		if (deq) begin
-			min = out_data;
-			$write ("%d\n", min);
-		end
-	end
-	
-	prio_q DUT (
-		.CLK(clk),
-		.rst_n(rst),
-		.enq (enq),
-		.deq (deq),
-		.inp_data(inp_data),
-		.out_data(out_data),
-		.count(count)
-	);
-	
-	
-	initial begin
-		clk = 1'b0;
-		rst = 1'b1;
-		#5
-		rst = 1'b0;
-		#10
-		rst = 1'b1;
-		
-		
-	end
-	
-	always
-		#5 clk = !clk;
+   reg                   clk;
+   reg                   rst_n;
+   reg                   enq;
+   reg                   deq;
+   reg                   done;
 
-integer f;		
-initial begin
-	#0;
-	f = $fopen("q_ops.txt");
-	$monitoron;
-	@(posedge done)
-	$monitoroff;
-	#10;
-	$fclose(f);
-end
+   reg     [`DWIDTH-1:0] inp_data;
+   wire    [`DWIDTH-1:0] out_data;
+   wire    [`HDEPTH-1:0] count;
 
-initial begin
-	$fmonitor(f, "clk:%d, enq: %d, deq:%d, in:%d, out:%d, count:%d",
-					clk, enq, deq, inp_data, out_data, count);
-end
-	
-initial begin
-	inp_data = 'd0;
-	enq = 'b0;
-	deq = 'b0;
-	done = 0;
-	
-	#20
-		
-@(negedge clk)
-	enq = 1;
-	deq = 0;
-	inp_data = 'd72;
-@(negedge clk)
-	enq = 1;
-	deq = 0;
-	inp_data = 'd44;
-@(negedge clk)
-	enq = 1;
-	deq = 0;
-	inp_data = 'd85;
-@(negedge clk)
-	enq = 0;
-	deq = 1;
-@(negedge clk)
-	enq = 0;
-	deq = 1;
-@(negedge clk)
-	enq = 1;
-	deq = 0;
-	inp_data = 'd43;
-@(negedge clk)
-	enq = 1;
-	deq = 0;
-	inp_data = 'd71;
-@(negedge clk)
-	enq = 1;
-	deq = 0;
-	inp_data = 'd15;
-@(negedge clk)
-	enq = 0;
-	deq = 1;
-@(negedge clk)
-	enq = 0;
-	deq = 1;
-@(negedge clk)
-	enq = 1;
-	deq = 0;
-	inp_data = 'd70;
-@(negedge clk)
-	enq = 1;
-	deq = 0;
-	inp_data = 'd91;
-@(negedge clk)
-	enq = 1;
-	deq = 0;
-	inp_data = 'd59;
-@(negedge clk)
-	enq = 0;
-	deq = 1;
-@(negedge clk)
-	enq = 0;
-	deq = 1;
-@(negedge clk)
-	enq = 1;
-	deq = 0;
-	inp_data = 'd74;
-@(negedge clk)
-	enq = 1;
-	deq = 0;
-	inp_data = 'd68;
-@(negedge clk)
-	enq = 1;
-	deq = 0;
-	inp_data = 'd27;
-@(negedge clk)
-	enq = 0;
-	deq = 1;
-@(negedge clk)
-	enq = 0;
-	deq = 1;
-@(negedge clk)
-	enq = 1;
-	deq = 0;
-	inp_data = 'd36;
-@(negedge clk)
-	enq = 1;
-	deq = 0;
-	inp_data = 'd24;
-@(negedge clk)
-	enq = 1;
-	deq = 0;
-	inp_data = 'd94;
-@(negedge clk)
-	enq = 0;
-	deq = 1;
-@(negedge clk)
-	enq = 0;
-	deq = 1;
-@(negedge clk)
-	enq = 1;
-	deq = 0;
-	inp_data = 'd37;
-@(negedge clk)
-	enq = 1;
-	deq = 0;
-	inp_data = 'd47;
-@(negedge clk)
-	enq = 1;
-	deq = 0;
-	inp_data = 'd58;
-@(negedge clk)
-	enq = 0;
-	deq = 1;
-@(negedge clk)
-	enq = 0;
-	deq = 1;
-@(negedge clk)
-	enq = 1;
-	deq = 0;
-	inp_data = 'd29;
-@(negedge clk)
-	enq = 1;
-	deq = 0;
-	inp_data = 'd89;
-@(negedge clk)
-	enq = 1;
-	deq = 0;
-	inp_data = 'd23;
-@(negedge clk)
-	enq = 0;
-	deq = 1;
-@(negedge clk)
-	enq = 0;
-	deq = 1;
-@(negedge clk)
-	enq = 1;
-	deq = 0;
-	inp_data = 'd86;
-@(negedge clk)
-	enq = 1;
-	deq = 0;
-	inp_data = 'd82;
-@(negedge clk)
-	enq = 1;
-	deq = 0;
-	inp_data = 'd98;
-@(negedge clk)
-	enq = 0;
-	deq = 1;
-@(negedge clk)
-	enq = 0;
-	deq = 1;
-@(negedge clk)
-	enq = 1;
-	deq = 0;
-	inp_data = 'd93;
-@(negedge clk)
-	enq = 1;
-	deq = 0;
-	inp_data = 'd20;
-@(negedge clk)
-	enq = 1;
-	deq = 0;
-	inp_data = 'd23;
-@(negedge clk)
-	enq = 0;
-	deq = 1;
-@(negedge clk)
-	enq = 0;
-	deq = 1;
-@(negedge clk)
-	enq = 1;
-	deq = 0;
-	inp_data = 'd65;
-@(negedge clk)
-	enq = 1;
-	deq = 0;
-	inp_data = 'd36;
-@(negedge clk)
-	enq = 1;
-	deq = 0;
-	inp_data = 'd17;
-@(negedge clk)
-	enq = 0;
-	deq = 1;
-@(negedge clk)
-	enq = 0;
-	deq = 1;
-@(negedge clk)
-	enq = 1;
-	deq = 0;
-	inp_data = 'd73;
-@(negedge clk)
-	enq = 1;
-	deq = 0;
-	inp_data = 'd83;
-@(negedge clk)
-	enq = 1;
-	deq = 0;
-	inp_data = 'd16;
-@(negedge clk)
-	enq = 0;
-	deq = 1;
-@(negedge clk)
-	enq = 0;
-	deq = 1;
-@(negedge clk)
-	enq = 1;
-	deq = 0;
-	inp_data = 'd60;
-@(negedge clk)
-	enq = 1;
-	deq = 0;
-	inp_data = 'd47;
-@(negedge clk)
-	enq = 1;
-	deq = 0;
-	inp_data = 'd27;
-@(negedge clk)
-	enq = 0;
-	deq = 1;
-@(negedge clk)
-	enq = 0;
-	deq = 1;
-@(negedge clk)
-	enq = 1;
-	deq = 0;
-	inp_data = 'd41;
-@(negedge clk)
-	enq = 1;
-	deq = 0;
-	inp_data = 'd87;
-@(negedge clk)
-	enq = 1;
-	deq = 0;
-	inp_data = 'd60;
-@(negedge clk)
-	enq = 0;
-	deq = 1;
-@(negedge clk)
-	enq = 0;
-	deq = 1;
-@(negedge clk)
-	enq = 1;
-	deq = 0;
-	inp_data = 'd76;
-@(negedge clk)
-	enq = 1;
-	deq = 0;
-	inp_data = 'd79;
-@(negedge clk)
-	enq = 0;
-	deq = 1;
-@(negedge clk)
-	enq = 1;
-	deq = 0;
-	inp_data = 'd98;
-@(negedge clk)
-	enq = 0;
-	deq = 1;
-@(negedge clk)
-	enq = 1;
-	deq = 0;
-	inp_data = 'd25;
-@(negedge clk)
-	enq = 0;
-	deq = 1;
-@(negedge clk)
-	enq = 0;
-	deq = 1;
-@(negedge clk)
-	enq = 0;
-	deq = 1;
-@(negedge clk)
-	enq = 1;
-	deq = 0;
-	inp_data = 'd16;
-@(negedge clk)
-	enq = 1;
-	deq = 0;
-	inp_data = 'd7;
-@(negedge clk)
-	enq = 0;
-	deq = 1;
-@(negedge clk)
-	enq = 0;
-	deq = 1;
-@(negedge clk)
-	enq = 0;
-	deq = 1;
-@(negedge clk)
-	enq = 1;
-	deq = 0;
-	inp_data = 'd22;
-@(negedge clk)
-	enq = 1;
-	deq = 0;
-	inp_data = 'd78;
-@(negedge clk)
-	enq = 0;
-	deq = 1;
-@(negedge clk)
-	enq = 0;
-	deq = 1;
-@(negedge clk)
-	enq = 0;
-	deq = 1;
-@(negedge clk)
-	enq = 1;
-	deq = 0;
-	inp_data = 'd45;
-@(negedge clk)
-	enq = 1;
-	deq = 0;
-	inp_data = 'd34;
-@(negedge clk)
-	enq = 0;
-	deq = 1;
-@(negedge clk)
-	enq = 0;
-	deq = 1;
-@(negedge clk)
-	enq = 0;
-	deq = 1;
-@(negedge clk)
-	enq = 1;
-	deq = 0;
-	inp_data = 'd28;
-@(negedge clk)
-	enq = 1;
-	deq = 0;
-	inp_data = 'd31;
-@(negedge clk)
-	enq = 0;
-	deq = 1;
-@(negedge clk)
-	enq = 0;
-	deq = 1;
-@(negedge clk)
-	enq = 0;
-	deq = 1;
-@(negedge clk)
-	enq = 1;
-	deq = 0;
-	inp_data = 'd21;
-@(negedge clk)
-	enq = 1;
-	deq = 0;
-	inp_data = 'd72;
-@(negedge clk)
-	enq = 0;
-	deq = 1;
-@(negedge clk)
-	enq = 0;
-	deq = 1;
-@(negedge clk)
-	enq = 0;
-	deq = 1;
-@(negedge clk)
-	enq = 1;
-	deq = 0;
-	inp_data = 'd98;
-@(negedge clk)
-	enq = 1;
-	deq = 0;
-	inp_data = 'd49;
-@(negedge clk)
-	enq = 0;
-	deq = 1;
-@(negedge clk)
-	enq = 0;
-	deq = 1;
-@(negedge clk)
-	enq = 0;
-	deq = 1;
-@(negedge clk)
-	enq = 1;
-	deq = 0;
-	inp_data = 'd54;
-@(negedge clk)
-	enq = 1;
-	deq = 0;
-	inp_data = 'd31;
-@(negedge clk)
-	enq = 0;
-	deq = 1;
-@(negedge clk)
-	enq = 0;
-	deq = 1;
-@(negedge clk)
-	enq = 0;
-	deq = 1;
-@(negedge clk)
-	enq = 1;
-	deq = 0;
-	inp_data = 'd96;
-@(negedge clk)
-	enq = 1;
-	deq = 0;
-	inp_data = 'd20;
-@(negedge clk)
-	enq = 0;
-	deq = 1;
-@(negedge clk)
-	enq = 0;
-	deq = 1;
-@(negedge clk)
-	enq = 0;
-	deq = 1;
-@(negedge clk)
-	enq = 1;
-	deq = 0;
-	inp_data = 'd42;
-@(negedge clk)
-	enq = 1;
-	deq = 0;
-	inp_data = 'd36;
-@(negedge clk)
-	enq = 0;
-	deq = 1;
-@(negedge clk)
-	enq = 0;
-	deq = 1;
-@(negedge clk)
-	enq = 0;
-	deq = 1;
-@(negedge clk)
-	enq = 1;
-	deq = 0;
-	inp_data = 'd63;
-@(negedge clk)
-	enq = 1;
-	deq = 0;
-	inp_data = 'd53;
-@(negedge clk)
-	enq = 0;
-	deq = 1;
-@(negedge clk)
-	enq = 0;
-	deq = 1;
-@(negedge clk)
-	enq = 0;
-	deq = 1;
-@(negedge clk)
-	enq = 1;
-	deq = 0;
-	inp_data = 'd14;
-@(negedge clk)
-	enq = 1;
-	deq = 0;
-	inp_data = 'd67;
-@(negedge clk)
-	enq = 0;
-	deq = 1;
-@(negedge clk)
-	enq = 0;
-	deq = 1;
-@(negedge clk)
-	enq = 0;
-	deq = 1;
-@(negedge clk)
-	enq = 1;
-	deq = 0;
-	inp_data = 'd25;
-@(negedge clk)
-	enq = 1;
-	deq = 0;
-	inp_data = 'd6;
-@(negedge clk)
-	enq = 0;
-	deq = 1;
-@(negedge clk)
-	enq = 0;
-	deq = 1;
-@(negedge clk)
-	enq = 0;
-	deq = 1;
-		
-@(negedge clk)
-	enq = 0;
-	deq = 0;
-@(negedge clk)	
-	done = 1;		
-end
-	
+   integer               data_file; // file handler
+   integer               scan_file; // file handler
+   integer               cycle_num; // Current clock cycle count;
+
+   initial begin
+      clk       = 1'b0;
+      rst_n     = 1'b0;
+      done      = 1'b0;
+      cycle_num = 0;
+
+      #20
+      rst_n     = 1'b1;
+
+      @(posedge done); // After all data has been read
+      rst_n     = 1'b0;
+
+      #10 $finish;
+   end
+
+   always
+      #5 clk = !clk;
+
+   always @(posedge clk) begin
+      cycle_num = cycle_num + 1;
+   end
+
+   initial begin // Read stimuli from data file
+      @(posedge rst_n);
+      data_file = $fopen("testbenches/prio_q_test_data.dat", "r");
+      if (data_file == 0) begin
+         $display("Error opening data file");
+         $finish;
+      end
+
+      // Read from file and drive test stimuli
+      @(posedge clk);
+      while (!$feof(data_file)) begin
+         // Input data patter : <enq>, <deq>, <data>
+         scan_file = $fscanf(data_file, "%b, %b, %d\n",
+            enq, deq, inp_data);
+         if(!scan_file) $display("Error reading data file");
+         @(posedge clk);
+      end
+
+      done      = 1'b1; // Finished reading all data
+      $fclose(data_file);
+   end
+
+   always @(posedge clk) begin
+      if(count == 16)
+         $display("Error: Invalid number of elements in the queue");
+      if(deq && inp_data != out_data)
+         $display("Dequeue mismatch: Cycle %d, Count %d, Expected %d, Actual %d",
+            cycle_num, count, inp_data, out_data);
+   end
+
+// Instantiate module under test
+   prio_q DUT (
+      .CLK(clk),
+      .rst_n(rst_n),
+      .enq (enq),
+      .deq (deq),
+      .inp_data({16'b0, inp_data}),
+      .out_data(out_data),
+      .count(count)
+   );
+
 endmodule
